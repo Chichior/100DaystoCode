cf51fc4ae904d0ad7e0654176b10ea8297264afd
Chichior <katiehakala@icloud.com>
2022-09-19 17:50:25 -0700

    Day One Complete!

--- Day_001/BandNameGenerator.py
+++ Day_001/BandNameGenerator.py
@@ -6,3 +6,5 @@ place = input("What is the name of the city you grew up in?\n")
 pet = input("What is the name of your pet?\n")
 
 print(input("Your band name should be " + place + " " + pet + "."))
+
+# pointless update
